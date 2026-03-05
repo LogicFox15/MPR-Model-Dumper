45ad5fdbfc7608b046c7749c10074d50f81caf0c
LogicFox15 <Hughesconnor1@gmail.com>
2026-03-05 11:41:16 -0700

    Update Program.cs

--- MetroidPrimeRemasterModelDumper/Program.cs
+++ MetroidPrimeRemasterModelDumper/Program.cs
@@ -1,5 +1,16 @@
-﻿
-using MetroidPrimeRemasterModelDumper;
+﻿using MetroidPrimeRemasterModelDumper;
+
+string manifest = AppContext.BaseDirectory + "/ModelManifest.json";
+
+if (!File.Exists(manifest))
+{
+    Console.WriteLine("The model manifest does not exist. Please provide the ROMFS directory so ");
+    Console.WriteLine("that a model manifest can be created. Please do not move the ROMFS once the");
+    Console.WriteLine("manifest is created, as the paths to the paks will be saved for future use.");
+    string romDir = Console.ReadLine();
+
+    ModelManifester.ProcessModels(romDir);
+}
 
 foreach (var arg in args)
 {
